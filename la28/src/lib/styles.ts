6e44c2c190ab37e5392092c7600b8a36f7331320
Mark <19700454+MokeEire@users.noreply.github.com>
2024-10-28 18:11:39 +0000

    Update styling

--- la28/src/lib/styles.ts
+++ la28/src/lib/styles.ts
@@ -4,4 +4,4 @@ export const hasMaptilerKey = PUBLIC_MAPTILER_KEY && PUBLIC_MAPTILER_KEY !== 'ke
 export const streetsStyle = `https://api.maptiler.com/maps/streets-v2/style.json?key=${PUBLIC_MAPTILER_KEY}`;
 
 export const mapClasses =
-  'relative w-[50%] aspect-[9/16] max-h-[560px] sm:max-h-[560px] sm:aspect-video mx-auto';
+  'relative w-[50%] min-w-[700px] aspect-[9/16] max-h-[560px] sm:max-h-[560px] sm:aspect-video mx-auto my-5';
--- la28/src/routes/+page.svelte
+++ la28/src/routes/+page.svelte
@@ -6,49 +6,51 @@
 
 	// source: https://en.wikipedia.org/wiki/Venues_of_the_2028_Summer_Olympics_and_Paralympics
 	import stadia from '$lib/stadia.json';
+	import railRoutes from '$lib/railRoutes.json';
 </script>
 
 <div class="app">
 	<Navbar />
-	<ul>
-		<li>
-			Where are the venues?
-			<ul>
-				<li>
-					Source: <a
-						href="https://en.wikipedia.org/wiki/Venues_of_the_2028_Summer_Olympics_and_Paralympics"
-						>Wikipedia</a
-					>
-				</li>
-				<li>What's the capacity of the venues?</li>
-			</ul>
-		</li>
-	</ul>
+	<div class="max-w-prose mx-auto">
+		<ul>
+			<li>
+				Where are the venues?
+				<ul class="list-disc list-inside">
+					<li>
+						Source: <a
+							href="https://en.wikipedia.org/wiki/Venues_of_the_2028_Summer_Olympics_and_Paralympics"
+							>Wikipedia</a
+						>
+					</li>
+					<li>What's the capacity of the venues?</li>
+				</ul>
+			</li>
+		</ul>
+		
+	</div>
 
-	<UsingMapLibre data={stadia} />
-	<!--<GetGTFSData />-->
+	<UsingMapLibre venues={stadia} routes={railRoutes} />
+		<!--<GetGTFSData />-->
 
-	<!--<UsingMapbox />-->
+		<!--<UsingMapbox />-->
+		<div class="max-w-prose mx-auto">
+		<ul>
+			<li>
+				How can people get to the venues?
+				<ul class="list-disc list-inside">
+					<li>Rail</li>
+					<li>Bus</li>
+					<li>Car</li>
+					<li>Walk</li>
+				</ul>
+			</li>
+		</ul>
 
-	<ul>
-		<li>
-			How can people get to the venues?
-			<ul>
-				<li>Rail</li>
-				<li>Bus</li>
-				<li>Car</li>
-				<li>Walk</li>
-			</ul>
-		</li>
-	</ul>
-
-	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-	</p>
+		<p>
+			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+		</p>
+		</div>
 </div>
 
 <style>
-	.app {
-		text-align: center;
-	}
 </style>
