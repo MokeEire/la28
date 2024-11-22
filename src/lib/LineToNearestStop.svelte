e3b83cc6238708c648987637e06e63be748220e9
Mark <19700454+MokeEire@users.noreply.github.com>
2024-11-22 20:30:22 +0000

    Add LineToNearestStop component to visualize nearest stop lines on the map

---
+++ src/lib/LineToNearestStop.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { PUBLIC_MAPTILER_KEY } from '$env/static/public';
+	import { MapLibre, Marker, Popup, DefaultMarker, GeoJSON, LineLayer } from 'svelte-maplibre';
+    import { lineString } from '@turf/turf';
+
+    export let venue;
+    let nearestStopLine = lineString([venue.venue_geometry.coordinates, venue.nearest_stop[0].geometry.coordinates])
+</script>
+
+<GeoJSON data={nearestStopLine}>
+    <LineLayer
+        layout={{ 'line-cap': 'round', 'line-join': 'round'}}
+        paint={{
+            'line-width': 3,
+            //'line-dasharray': [5, 2],
+            'line-color': '#000',
+            'line-opacity': 0.8,
+          }}
+        />
+</GeoJSON>
--- src/lib/UsingSvelteLibre.svelte
+++ src/lib/UsingSvelteLibre.svelte
@@ -2,10 +2,14 @@
 	import { PUBLIC_MAPTILER_KEY } from '$env/static/public';
 	import { MapLibre, Marker, Popup, DefaultMarker, GeoJSON, LineLayer } from 'svelte-maplibre';
 	import { mapClasses } from '$lib/styles';
-
+	//import {lineString} from '@turf/turf';
+	import LineToNearestStop from '$lib/LineToNearestStop.svelte';
 	export let venues;
 	export let metroRoutes;
 	export let metroLinkRoutes;
+
+	//console.log(venues[0])
+	//console.log(lineString([venues[0].venue_geometry.coordinates, venues[0].nearest_stop[0].geometry.coordinates]))
 </script>
 
 <MapLibre
@@ -35,10 +39,16 @@
             <div class="text-lg font-bold">{venue.venue}</div>
 			<p>Events: {venue.events}</p>
 			{#each venue.sources as source, i}
-				<a href={source}>Source #{i+1}</a>
+				<a class="block text-right" href={source}>Source #{i+1}</a>
 			{/each}
         </Popup>
         </Marker>
+		<Marker
+			lngLat={venue.nearest_stop[0].geometry.coordinates}
+			class="grid h-4 w-4 place-items-center rounded-full border border-gray-200 text-black shadow-2xl focus:outline-2 focus:outline-black bg-red-400/80"
+		/>
+		<LineToNearestStop venue={venue} />
+		
 	{/each}
 	<GeoJSON id="metro-rail" data={metroRoutes}>
 		<LineLayer
