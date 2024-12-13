304e61bcf711ea6ab5028ebb14634c8f5cbc2efa
Mark <19700454+MokeEire@users.noreply.github.com>
2024-12-13 00:24:49 +0000

    Update Navbar heading and adjust styles for improved readability

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,12 +1,12 @@
 <div class="heading">
-    <h1>How will LA pull off a car-free Olympics in 2028?</h1>
+    <h1>Will LA pull off a car-free Olympics in 2028?</h1>
   </div>
   
   <style>
     .heading {
       margin: 0;
       padding: 0px;
-      background-color: black;
+      background-color: #9418dc;
       color: white;
       text-align: center;
     }
