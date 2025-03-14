4cad160fe089b9d10f6ae21193a7aa57ffdd0c46
Mark B <markkbarrett@gmail.com>
2025-03-14 16:36:03 -0700

    Add Legend components

---
+++ src/lib/Legend.svelte
@@ -0,0 +1,20 @@
+<script>
+    // Declare props using $props
+  let { legend_data, legend_color_function, legend_label_array, width } = $props();
+</script>
+
+    {#each legend_data as time, i}
+            <!-- Color box -->
+            <rect
+                style="border-radius:10px;"
+                width="16"
+                height="16"
+                x={width/3+(i * 100)}
+                y="4"
+                fill={legend_color_function(time)}
+            />
+            <!-- Category text -->
+            <text class="fill-gray-800" x={width/3+(64+ i * 100)} y="16" font-size="14px" alignment-baseline="middle"
+                >{legend_label_array[time]} mins</text
+            >
+{/each}
---
+++ src/lib/LegendHTML.svelte
@@ -0,0 +1,39 @@
+<script>
+	// Declare props using $props
+	let { legend_data, legend_color_function, legend_label_array, width } = $props();
+</script>
+
+<div class="legend-container mx-4 opacity-90">
+	{#each legend_data as time, i}
+		<div class="legend-item">
+			<!-- Color box -->
+			<div class="legend-rect" style="background-color: {legend_color_function(time)};"></div>
+			<!-- Category text -->
+			<span class="legend-text">{legend_label_array[time]} mins</span>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.legend-container {
+		display: flex;
+		justify-content: flex-end;
+        gap: 16px;
+	}
+
+    .legend-item {
+        display: flex;
+        align-items: center;
+        gap: 8px
+    }
+	.legend-rect {
+		width: 16px;
+		height: 16px;
+		border-radius: 10px;
+	}
+	.legend-text {
+		font-size: 14px;
+		color: var(--color-text);
+		vertical-align: middle;
+	}
+</style>
